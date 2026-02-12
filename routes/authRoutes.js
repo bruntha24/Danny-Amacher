380ff9134973925b4ee4545b028f179771cb45f5
bruntha24 <m.bruntha24@gmail.com>
2026-02-12 17:22:24 +0530

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+const router = express.Router();
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
